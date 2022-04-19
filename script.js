4791f6ef973cefd305fdd3af9fd0e1afbb670012
Nishanth artham <arthamnishanth123@gmail.com>
2022-04-19 19:32:34 +0530

    Added Javascript

--- script.js
+++ script.js
@@ -4,3 +4,25 @@ $('#ripple').ripples({
 	dropRadius: 20,
 	perturbance: 0.01,
 });
+const img1 = document.getElementById('img1');
+const img2 = document.getElementById('img2');
+const img3 = document.getElementById('img3');
+// Tag.addEventListener('mouseleave',changeclass())
+function changeclass1() {
+	img1.src = "./images/web-development-color.png";
+}
+function Originalclass1() {
+	img1.src = "./images/web-development.png";
+}
+function changeclass2() {
+	img2.src = "./images/web-development-color.png";
+}
+function Originalclass2() {
+	img2.src = "./images/web-development.png";
+}
+function changeclass3() {
+	img3.src = "./images/web-development-color.png";
+}
+function Originalclass3() {
+	img3.src = "./images/web-development.png";
+}
